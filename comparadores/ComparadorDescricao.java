46a3dc5ab3df3ee20f9cddb909054e559ee45d43
Gabriel <gabrielbbeckedorff@gmail.com>
2025-07-03 18:41:51 -0300

    Alterações package coomparadores

--- comparadores/ComparadorDescricao.java
+++ comparadores/ComparadorDescricao.java
@@ -1,11 +1,10 @@
 package comparadores;
 import java.util.Comparator;
-
 import modelo.Produto;
 public class ComparadorDescricao implements Comparator<Produto> {
 
     public int compare(Produto p1, Produto p2) {
-        return p1.getDescricao().compareTo(p2.getDescricao());
+        return p1.getDescricao().compareToIgnoreCase(p2.getDescricao());
     }
 }
 
