d1306e40a13704fc48c7c0ceaab9ff9192a9064f
Matheus_Note <matheussgustavoo@gmail.com>
2025-07-06 11:00:02 -0300

    remoção de linhas e arquivos nulos

--- CarregarProdutos.java
+++ CarregarProdutos.java
@@ -9,6 +9,7 @@ import modelo.ProdutoPadrao;
 public class CarregarProdutos {
     public String caminho;
     public HashMap<Integer, ArrayList<String>> formatacao;
+
     public CarregarProdutos(String caminho) {
         this.caminho = caminho;
     }
@@ -16,8 +17,7 @@ public class CarregarProdutos {
     public ArrayList<Produto> carregar() {
         ArrayList<Produto> produtos = new ArrayList<>();
 
-        try {
-            Scanner linha = new Scanner(new File(caminho));
+        try (Scanner linha = new Scanner(new File(caminho));) {
 
             // Ignorando a primeira linha
             if (linha.hasNextLine()) {
@@ -33,7 +33,7 @@ public class CarregarProdutos {
                 int qtdEstoque = Integer.parseInt(campos[3].trim());
                 double preco = Double.parseDouble(campos[4].trim());
 
-                produtos.add(new ProdutoPadrao(id, descricao, categoria, qtdEstoque, preco)); // Assumindo que você tenha esse construtor
+                produtos.add(new ProdutoPadrao(id, descricao, categoria, qtdEstoque, preco));
             }
 
         } catch (FileNotFoundException e) {
--- Particionador_Interface.java
+++
@@ -1,3 +0,0 @@
-public interface Particionador_Interface {
-    
-}
