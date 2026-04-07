ab014e7907c2e2f0ccebd40ea32ee6daa271562f
Matheus_Note <matheussgustavoo@gmail.com>
2026-04-07 16:17:25 -0300

    refactor: improve formatting and structure in ComparadorDescricao, ComparadorEstoque, and ComparadorPreco classes

--- comparadores/ComparadorDescricao.java
+++ comparadores/ComparadorDescricao.java
@@ -1,10 +1,12 @@
 package comparadores;
+
 import java.util.Comparator;
 import modelo.Produto;
+
 public class ComparadorDescricao implements Comparator<Produto> {
 
+    @Override
     public int compare(Produto p1, Produto p2) {
         return p1.getDescricao().compareToIgnoreCase(p2.getDescricao());
     }
 }
-
--- comparadores/ComparadorPreco.java
+++ comparadores/ComparadorPreco.java
@@ -1,8 +1,11 @@
 package comparadores;
-import java.util.Comparator;
 
+import java.util.Comparator;
 import modelo.Produto;
-public  class ComparadorPreco implements Comparator<Produto> {
+
+public class ComparadorPreco implements Comparator<Produto> {
+
+    @Override
     public int compare(Produto p1, Produto p2) {
         return Double.compare(p1.getPreco(), p2.getPreco());
     }
